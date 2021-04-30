0018810cce88f5702eee1961b3ca8eb832c07744
Weston Knowlton <kno18012@byui.edu>
2021-04-29 21:32:19 -0600

    team activity 02

--- routes/ta02.js
+++ routes/ta02.js
@@ -3,12 +3,29 @@
 const express = require('express');
 const router = express.Router();
 
+const users = [];
+
+router.post('/addUser', (req, res, next) => {
+    users.push(req.body.username);
+    res.redirect('/ta02/');
+});
+
+router.post('/removeUser', (req, res, next) => {
+    users.forEach((user) =>{
+        if(user == req.body.usernameRemove){
+            users.splice(users.indexOf(user), 1);
+        }
+    });
+    res.redirect('/ta02/');
+});
+
 router.get('/',(req, res, next) => {
     res.render('pages/ta02', { 
         title: 'Team Activity 02', 
         path: '/ta02', // For pug, EJS 
         activeTA03: true, // For HBS
         contentCSS: true, // For HBS
+        users: users
     });
 });
 
